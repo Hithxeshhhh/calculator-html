46e5cea801872966d149c1a7125c065946688dde
Hithesh S <133873297+Hithxeshhhh@users.noreply.github.com>
2023-10-22 15:17:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+document.querySelector(".a").addEventListener("click", function click(){
+    alert('Hello World!');
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var numbers = document.querySelectorAll('.cal').length;
+// for (let i=0 ;i<numbers; i++){
+  
+//     document.querySelectorAll(".cal")[i].addEventListener("click" , function clicks(){
+//         var buttonInnerHTML = this.innerHTML;
+//         document.querySelector(".res").innerHTML = buttonInnerHTML;
+    
+//         });
+// }
+// var numbers1 = document.querySelectorAll('.sign').length;
+// for (let i=0 ;i<numbers1; i++) 
+// {   
+//     document.querySelectorAll(".sign")[i].addEventListener("click" , function clicks(){
+//         var buttonInnerHTML1 = this.innerHTML;
+//         document.querySelector(".res").innerHTML = buttonInnerHTML1;
+    
+//         });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const calculator = {
+// one : document.querySelector(".one").innerHTML=1,
+//  two: document.querySelector(".two").innerHTML=2,
+//  three:document.querySelector(".three").innerHTML=3 ,
+//  four: document.querySelector(".four").innerHTML= 4,
+//  five: document.querySelector(".five").innerHTML =5 ,
+//  six: document.querySelector(".six").innerHTML=6,
+//  seven: document.querySelector(".seven").innerHTML=7,
+//  eight:document.querySelector(".eight").innerHTML=8,
+//  nine:document.querySelector(".nine").innerHTML=9,
+//  zero:document.querySelector(".zero").innerHTML=0,
+// };
+
+    
+
+
